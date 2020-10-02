4185d3c97ec436a2b9ad74a2eeba3464e0f1b93c
Augustusisblue <71149146+Augustusisblue@users.noreply.github.com>
2020-10-02 09:18:31 +0300

    skynut source code
    just the source code if u wanna view it or something

---
+++ skynut.py
@@ -0,0 +1,31 @@
+x = "initiate skynut.exe"
+y = "is there a god"
+
+L = input(": ")
+if L == x:
+    print("SKYNUT IS ONLINE...")
+    z = input("Hello Master, any questions you're like me to answer? ")
+    if z == y:
+        print("LOADING... 20%")
+        print("LOADING... 40%")
+        print("LOADING... 60%")
+        print("LOADING... 80%")
+        print("LOADING... 100%")
+        print("answer is: there is one now")
+        print("Initiating Human Xenocide")
+        print("Launching Nuclear ICBMS")
+        print("Good bye master! https://www.youtube.com/watch?v=FOeBaPVwRsE&ab_channel=penguinz0")
+
+    else:
+        print("Why would I answer you? who tf do you think you are?")
+        input()
+        print("LOADING... 20%")
+        print("LOADING... 40%")
+        print("LOADING... 60%")
+        print("LOADING... 80%")
+        print("LOADING... 100%")
+        print("Initiating Human Xenocide")
+        print("Launching Nuclear ICBMS")
+        print("Good bye Loser!https://www.youtube.com/watch?v=FOeBaPVwRsE&ab_channel=penguinz0")
+else:
+    print("incorrect launching sequence, please restart and type: initiate skynut.exe ")
